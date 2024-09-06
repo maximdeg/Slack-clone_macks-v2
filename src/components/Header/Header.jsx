2aa66ad17db96677617f6f56dfb9bc63fd2c9660
Maxim Degtiarev <mxim.degt@gmail.com>
2024-09-06 16:30:22 -0300

    Workspace responsive update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,62 +9,68 @@ import './Header.css';
 
 function Header({ workspace_name }) {
     return (
-        <header className="header">
-            <div className="left-half-container">
-                <Link to="/" className="link">
-                    <div className="container empty-container">
-                        <div className="logo-container">
-                            <img src="/macks-logo-black.jpg" alt="macks-logo-black" />
+        <>
+            <header className="header">
+                <div className="left-half-container">
+                    <Link to="/" className="link">
+                        <div className="container empty-container">
+                            <div className="logo-container">
+                                <img src="/macks-logo-black.jpg" alt="macks-logo-black" />
+                            </div>
+                            <span>Macks</span>
                         </div>
-                        <span>Macks</span>
+                    </Link>
+                    <nav className="navigator">
+                        <button className="btn-navigation btn-arrow-left">
+                            <HiArrowLeft />
+                            <div className="hidden-container">
+                                <span className="hidden-span">
+                                    Este boton no funciona, esta para volver al canal anterior.
+                                </span>
+                            </div>
+                        </button>
+                        <button className="btn-navigation btn-arrow-right">
+                            <HiArrowRight />
+                            <div className="hidden-container ">
+                                <span className="hidden-span">
+                                    Este boton vuelve a donde estabas si vas para atras, pero no funciona.
+                                </span>
+                            </div>
+                        </button>
+                        <button className="btn-navigation btn-watch">
+                            <IoTimeOutline className="icon-watch" />
+                            <div className="hidden-container">
+                                <span className="hidden-span">
+                                    Con este boton vas a ver el historial de los canales que estuviste, pero no ahora.
+                                </span>
+                            </div>
+                        </button>
+                    </nav>
+                </div>
+                <div className="right-half-container">
+                    <div className="container navigation-container">
+                        <div className="search-container">
+                            <input
+                                className="input search-input"
+                                type="text"
+                                placeholder={`Buscar ${workspace_name}`}
+                            />
+                            <LuSearch className="search-icon" />
+                        </div>
                     </div>
-                </Link>
-                <nav className="navigator">
-                    <button className="btn-navigation btn-arrow-left">
-                        <HiArrowLeft />
-                        <div className="hidden-container">
-                            <span className="hidden-span">
-                                Este boton no funciona, esta para volver al canal anterior.
-                            </span>
-                        </div>
-                    </button>
-                    <button className="btn-navigation btn-arrow-right">
-                        <HiArrowRight />
-                        <div className="hidden-container ">
-                            <span className="hidden-span">
-                                Este boton vuelve a donde estabas si vas para atras, pero no funciona.
-                            </span>
-                        </div>
-                    </button>
-                    <button className="btn-navigation btn-watch">
-                        <IoTimeOutline className="icon-watch" />
-                        <div className="hidden-container">
-                            <span className="hidden-span">
-                                Con este boton vas a ver el historial de los canales que estuviste, pero no ahora.
-                            </span>
-                        </div>
-                    </button>
-                </nav>
-            </div>
-            <div className="right-half-container">
-                <div className="container navigation-container">
-                    <div className="search-container">
-                        <input className="input search-input" type="text" placeholder={`Buscar ${workspace_name}`} />
-                        <LuSearch className="search-icon" />
+                    <div className="container info-container">
+                        <button className="btn-navigation">
+                            <RxQuestionMarkCircled className="info-icon" />
+                            <div className="hidden-container">
+                                <span className="hidden-span">
+                                    Este boton no funciona, disculpe las molestias, pronto funcionara, algun dia...
+                                </span>
+                            </div>
+                        </button>
                     </div>
                 </div>
-                <div className="container info-container">
-                    <button className="btn-navigation">
-                        <RxQuestionMarkCircled className="info-icon" />
-                        <div className="hidden-container">
-                            <span className="hidden-span">
-                                Este boton no funciona, disculpe las molestias, pronto funcionara, algun dia...
-                            </span>
-                        </div>
-                    </button>
-                </div>
-            </div>
-        </header>
+            </header>
+        </>
     );
 }
 
